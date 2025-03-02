3f5fd12af2c412dba6236a91ac4deedbd195b489
CamiloGG <johancamilogonzalez@ucundinamrca.edu.co>
2025-03-02 11:43:36 -0500

    Definimos dos enteros los cuales usamos para calcular por cada tipo de operador

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,8 +1,23 @@
 public class Operadores {
     //Operadores
+    int a=8;
+    int b=2;
     //Aritmeticos: +, -, *, /, %
-
-    //Relacionales: ==, !=, >, <, >=, <=
-
+        int Suma=a+b;
+        int resta=a-b;
+        int Mult=a*b;
+        int Div=a/b;
+        int Mod=a%b;
+    //Relacionales: ==, !=, >, <, >=, <= estos de aqui se manejan con el tipo de dato booleano
+        boolean Igual=a==b;
+        boolean Diferente=a!=b;
+        boolean Mayor=a>b;
+        boolean Menor=a<b;
+        boolean MayorIgual=a>=b;
+        boolean MenorIgual=a<=b;
     //Logicos: &&, ||, !
+        boolean And=(a<b)&&(b>a);
+        boolean Or=(a!=0)||(b==0);
+        boolean Negacion=(a!=0)&&!(b==0);
+
 }
