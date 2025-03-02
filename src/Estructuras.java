c9a389dcb41404aae48b5a7e2ee0f1411cc2d6e8
JuanPRam <Juanpramirez@ucundinamarca.edu.co>
2025-03-02 11:19:09 -0500

    HAcemos los ejemplos de las estructuras condicionales

--- src/Estructuras.java
+++ src/Estructuras.java
@@ -2,7 +2,22 @@ public class Estructuras {
     //Estructuras de Control
         //Condicionales
             //if -else
+            int edad=18;
+            if (edad>=18){
+                System.out.println("usted es mayor de edad");
+            }else{
+                System.out.println("Usted es menor de edad");
+            }
             //switch
+            int Opc =3;
+            swicht(Opc){
+                case 1:
+                    System.out.println("Opcion 1");
+                case 2:
+                    System.out.println("Opcion 2");
+                default :
+                    System.out.println("Opcion invalida");
+            }
         //Bucles o ciclos
             //ciclo for
             //ciclo while
