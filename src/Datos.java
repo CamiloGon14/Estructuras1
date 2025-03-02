f945b375ce670ccfe517da20eeceae1a0a31dca2
CamiloGG <johancamilogonzalez@ucundinamrca.edu.co>
2025-03-01 21:50:26 -0500

    Se hizo lo que corresponde a los ejemplo de los datos primitivos enteros

--- src/Datos.java
+++ src/Datos.java
@@ -1,3 +1,15 @@
 public class Datos {
-
+//Datos Primitivos
+    //Enteros
+        byte OchoBits =127; //Bite
+        short DiesciseisBits = 32767;//short
+        int TreintaydosBits = 2147483647;//int
+        long SesentaycuatroBits =9223372036;//long
+    //Punto Flotante
+        //float
+        //double
+    //Caracter
+        //char
+    //Booleano
+        //boolean
 }
