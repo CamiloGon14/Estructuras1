78933273a016a8f0f1fa8da9e75b9fe270a33639
JuanPRam <Juanpramirez@ucundinamarca.edu.co>
2025-03-02 10:58:17 -0500

    Se creo la clase de estructuras y se definio los items a tratar

---
+++ src/Estructuras.java
@@ -0,0 +1,9 @@
+public class Estructuras {
+    //Estructuras de Control
+        //Condicionales
+            //if -else
+            //switch
+        //Bucles o ciclos
+            //ciclo for
+            //ciclo while
+}
