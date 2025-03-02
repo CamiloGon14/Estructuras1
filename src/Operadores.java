f02a45e6ed27909863f55215e55fac785944d407
CamiloGG <johancamilogonzalez@ucundinamrca.edu.co>
2025-03-02 11:50:17 -0500

    Se realizo una forma en que cada datos se muestre por consola

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,23 +1,41 @@
 public class Operadores {
-    //Operadores
-    int a=8;
-    int b=2;
-    //Aritmeticos: +, -, *, /, %
-        int Suma=a+b;
-        int resta=a-b;
-        int Mult=a*b;
-        int Div=a/b;
-        int Mod=a%b;
-    //Relacionales: ==, !=, >, <, >=, <= estos de aqui se manejan con el tipo de dato booleano
-        boolean Igual=a==b;
-        boolean Diferente=a!=b;
-        boolean Mayor=a>b;
-        boolean Menor=a<b;
-        boolean MayorIgual=a>=b;
-        boolean MenorIgual=a<=b;
-    //Logicos: &&, ||, !
-        boolean And=(a<b)&&(b>a);
-        boolean Or=(a!=0)||(b==0);
-        boolean Negacion=(a!=0)&&!(b==0);
+    public static void main(String[] args) {
+        // Operadores
+        int a = 8;
+        int b = 2;
 
+        // Aritméticos: +, -, *, /, %
+        int Suma = a + b;
+        System.out.println("Suma: " + Suma);
+        int Resta = a - b;
+        System.out.println("Resta: " + Resta);
+        int Mult = a * b;
+        System.out.println("Multiplicación: " + Mult);
+        int Div = a / b;
+        System.out.println("División: " + Div);
+        int Mod = a % b;
+        System.out.println("Módulo: " + Mod)
+
+        // Relacionales: ==, !=, >, <, >=, <=
+        boolean Igual = a == b;
+        System.out.println("Igual: " + Igual);
+        boolean Diferente = a != b;
+        System.out.println("Diferente: " + Diferente);
+        boolean Mayor = a > b;
+        System.out.println("Mayor: " + Mayor);
+        boolean Menor = a < b;
+        System.out.println("Menor: " + Menor);
+        boolean MayorIgual = a >= b;
+        System.out.println("Mayor Igual: " + MayorIgual);
+        boolean MenorIgual = a <= b;
+        System.out.println("Menor Igual: " + MenorIgual);
+
+        // Lógicos: &&, ||, !
+        boolean And = (a < b) && (b > a);
+        System.out.println("Operador AND: " + And);
+        boolean Or = (a != 0) || (b == 0);
+        System.out.println("Operador OR: " + Or);
+        boolean Negacion = (a != 0) && !(b == 0);
+        System.out.println("Operador NOT: " + Negacion);
+    }
 }
